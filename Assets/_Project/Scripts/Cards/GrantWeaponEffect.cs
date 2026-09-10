1ea84f7f79860a12ee87a5c371b342be65fe0bed
YanB <yanisbou324@gmail.com>
2026-09-10 11:11:55 +0200

    [ADD] WeaponDefinition/WeaponSlot

--- Assets/_Project/Scripts/Cards/GrantWeaponEffect.cs
+++ Assets/_Project/Scripts/Cards/GrantWeaponEffect.cs
@@ -7,11 +7,13 @@ namespace Game
 
 public class GrantWeaponEffect : CardEffect
 {
+    WeaponSlot weaponSlot = new WeaponSlot(); 
     public override void Apply(Actor Target)
     {
-        //Core Script pour l'obtention d'une arme au joueur 
+        //Core Script pour l'obtention d'une arme au joueur
+        weaponSlot.weaponEquipped = weapon;
     }
-    //private WeaponDefinition weapon; TODO retirer les comm
+    private WeaponDefinition weapon;
 }
 
 }
